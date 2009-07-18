be8196f3aacf16bc1af2950299c8381d15f673df
Hylke Bons <hylke.bons@intel.com>
2009-07-18 13:14:50 +0100

    Don't run app when there are compiler errors.

--- run.sh
+++ run.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-gmcs main.cs -pkg:gtk-sharp-2.0 -r:System.Data.dll -r:Mono.Data.SqliteClient.dll
-mono main.exe 
+gmcs main.cs -pkg:gtk-sharp-2.0 -r:System.Data.dll -r:Mono.Data.SqliteClient.dll && mono main.exe 
